11d98374d9af29b19d8d9dca1f108fa91efcad0c
adel <adelgahgouh@gmail.com>
2020-07-24 11:02:17 +0100

    add splash screen

---
+++ app/src/main/java/com/treecode/tassbih/splash.java
@@ -0,0 +1,50 @@
+package com.treecode.tassbih;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+
+public class splash extends AppCompatActivity {
+int periode=3000;
+    MediaPlayer mp;
+PrefManager prefManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        prefManager=new PrefManager(getApplicationContext());
+        mp=MediaPlayer.create(getApplicationContext(),R.raw.intro);
+       boolean b=prefManager.getsoundIntro();
+       if(b)
+        mp.start();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, periode);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mp.isPlaying())
+            mp.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mp.isPlaying())
+        mp.stop();
+    }
+}
